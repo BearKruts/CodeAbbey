242d49535c42258fa07d0c825638ac4cce69deea
Mykhailo Kruts <bear.kruts@gmail.com>
2018-03-19 13:53:47 +0200

    Solved problem "Rock Paper Scissors"

---
+++ Java/RockPaperScissors.java
@@ -0,0 +1,38 @@
+/**
+*    Problem: Rock Paper Scissors
+*    Url: http://www.codeabbey.com/index/task_view/rock-paper-scissors
+*    Autor: Mykhailo Kruts
+*    Date: 18 March 2018
+*/
+
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+        final int AMOUNT_OF_PAIRS = Integer.parseInt(reader.nextLine());
+
+        for (int i = 0; i < AMOUNT_OF_PAIRS; i++) {
+        	int[] player = {0, 0};
+
+            for (String el : reader.nextLine().split(" ")) {
+            	switch (el) {
+            		case "RS":
+            		case "PR":
+            		case "SP":
+            			player[0]++;
+            			break;
+            		case "SR":
+            		case "RP":
+            		case "PS":
+            			player[1]++;
+            			break;
+            	}
+            }
+           
+            System.out.printf("%d ", (player[0] > player[1]) ? 1 : 2);
+        }
+        
+        reader.close();
+    }
+}
